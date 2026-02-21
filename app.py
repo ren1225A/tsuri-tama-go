6164beb6193f59caf909693be3c48ba5d5eb17bc
ren1225A <0se3261v3691d9u@gmail.com>
2026-02-21 22:19:07 +0900

    トップ画面編集中

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask_login import LoginManager
+from flask import Flask, render_template
+from flask_login import LoginManager, current_user
 from dotenv import load_dotenv
 import os
 
@@ -15,6 +15,12 @@ db.init_app(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'auth.login'
 
+from models import db, User
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 from routes.auth import auth_bp
 from routes.learn import learn_bp
 from routes.mission import mission_bp
@@ -22,6 +28,11 @@ from routes.mission import mission_bp
 app.register_blueprint(auth_bp)
 app.register_blueprint(learn_bp)
 app.register_blueprint(mission_bp)
+
+@app.route('/')
+def index():
+    best_catch = None
+    return render_template('index.html', best_catch=best_catch)
 
 with app.app_context():
     db.create_all()
