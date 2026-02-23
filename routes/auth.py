20e0f730d4b9d0123bd38225ab03219fe9f760af
masumin1000 <masumin1000@gmail.com>
2026-02-23 16:11:31 +0900

    23日変更点

--- routes/auth.py
+++ routes/auth.py
@@ -1,11 +1,34 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, url_for
+from flask_login import login_user
+from models import User
 
-auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
+auth_bp = Blueprint('auth', __name__)
 
-@auth_bp.route('/login')
+@auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
-    return 'ログインページ（Aさんが作る）'
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
 
-@auth_bp.route('/register')
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            login_user(user)
+            return redirect(url_for('quest.show_quests'))
+
+        return "ログイン失敗"
+
+    return render_template('login.html')
+@auth_bp.route('/register', methods=['GET', 'POST'])
 def register():
-    return '登録ページ（Aさんが作る）'
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('auth.login'))
+
+    return render_template('register.html')    
