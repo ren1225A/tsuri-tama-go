8e4fe80d8eddf8f60557922547d751730f2d0ea2
masumin1000 <masumin1000@gmail.com>
2026-02-23 21:57:02 +0900

    23日夜変更

--- routes/auth.py
+++ routes/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_user
 from models import User
 
-auth_bp = Blueprint('auth', __name__)
+auth_bp = Blueprint('auth', __name__,url_prefix='/auth'  )
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
@@ -25,8 +25,13 @@ def register():
         username = request.form['username']
         password = request.form['password']
 
-        user = User(username=username, password=password)
-        db.session.add(user)
+         # 既に存在するかチェック
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            return "そのユーザー名は既に使われています"
+
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
         db.session.commit()
 
         return redirect(url_for('auth.login'))
